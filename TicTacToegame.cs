0b4754b47be64fedf55977760b991f23f6d36e31
Ruthikreddy <ruthikreddy15@gmail.com>
2020-10-08 10:17:30 +0530

    [Ruthik Push for UC1]

---
+++ TicTacToegame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+
+    class TicTacToeGame
+    {
+        
+       
+        public void InitializeBoard()
+        {
+            char[] board = new char[10];
+
+            for (int i = 1; i < 10; i++)
+                board[i] = ' ';
+        }
+
+
+    }
+}
